47ac18df61a42fd56b934d9156f23f23fe63f535
sbh69840 <sbh69840@gmail.com>
2019-11-29 21:00:05 +0530

    Added the basic layout of the game
    There is a matrix on the screen created using the class cell

--- scripts/sketch.js
+++ scripts/sketch.js
@@ -1,12 +1,45 @@
+let rows = 30;
+let columns = 30;
+let width_cell = 10;
+let height_cell = 10;
+let grid = [];
 function setup() {
-  createCanvas(640, 480);
+  createCanvas(740, 880);
+  for(let i=0;i<rows;i++){
+    for(let j =0;j<columns;j++){
+      grid[i*rows+j] = new cell(i+i*width_cell,j+j*height_cell,width_cell,height_cell);
+    }
+  }
+  console.log(grid[0]);
 }
 
 function draw() {
-  if (mouseIsPressed) {
-    fill(0);
-  } else {
-    fill(255);
+  background(0);
+  noStroke();
+  fill(255);
+  for(var i=0;i<rows;i++){
+    for(var j=0;j<columns;j++){
+      grid[i*rows+j].display_white();
+    }
   }
-  ellipse(mouseX, mouseY, 80, 80);
+  grid[5*rows+5].display_black();
+
+}
+class cell{
+  constructor(x,y,w,h){
+    this.x = x;
+    this.y = y;
+    this.w = w;
+    this.h = h;
+  }
+  display_white(){
+    noStroke();
+    fill(255);
+    rect(this.x,this.y,this.w,this.h);
+  }
+  display_black(){
+    noStroke();
+    fill(0);
+    rect(this.x,this.y,this.w,this.h);
+  }
 }
